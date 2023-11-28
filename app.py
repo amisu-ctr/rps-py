416c203e8ee9ec97baaeeb016cf137318883b992
amisu <hamisurahman@gmail.com>
2023-11-28 13:30:58 +0000

    Comuter random selection

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
-print("Rock..")
-print("Paper...")
-print("Scissors..")
+import random
 
-player1 = input("Player 1, make your move: ")
-print("***NO CHEATING!\n\n" * 20)
-player2 = input("Player 2, make your move: ")
+# print("Rock..")
+# print("Paper...")
+# print("Scissors..")
 
-if player1 == player2:
-	print("It's a tie!")
-elif player1 == "rock":
-	if player2 == "scissors":
-		print("player1 wins!")
-	elif player2 == "paper":
-		print("player2 wins!")
-elif player1 == "paper":
-	if player2 == "rock":
-		print("player1 wins!")
-	elif player2 == "scissors":
-		print("player2 wins!")
-elif player1 == "scissors":
-	if player2 == "paper":
-		print("player1 wins!")
-	elif player2 == "rock":
-		print("player2 wins!")	
+# player1 = input("Player 1, make your move: ")
+rand_num = random.randint(0,2)
+if rand_num == 0:
+    computer = 'rock'
+elif rand_num == 1:
+    computer = 'paper'
 else:
-	print("something went wrong")
+    computer = 'scissors'
+print(computer)
+
+
+# player2 = input("Player 2, make your move: ")
+
+# if player1 == player2:
+# 	print("It's a tie!")
+# elif player1 == "rock":
+# 	if player2 == "scissors":
+# 		print("player1 wins!")
+# 	elif player2 == "paper":
+# 		print("player2 wins!")
+# elif player1 == "paper":
+# 	if player2 == "rock":
+# 		print("player1 wins!")
+# 	elif player2 == "scissors":
+# 		print("player2 wins!")
+# elif player1 == "scissors":
+# 	if player2 == "paper":
+# 		print("player1 wins!")
+# 	elif player2 == "rock":
+# 		print("player2 wins!")	
+# else:
+# 	print("something went wrong")
 
