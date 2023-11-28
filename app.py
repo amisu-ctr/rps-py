ea931a88bf76da19bfc5f1cf4a6bbfd2043ab42e
amisu <hamisurahman@gmail.com>
2023-11-28 13:09:51 +0000

    Player 1 and Player 2 input variables

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
-# import the Data
-# Clean the Data
-# Split the Data into Training/Test Sets
-# Create a Model 
-# Train the Model 
-# Make the Predictions
-# Evaluate and improve 
+print("Rock..")
+print("Paper...")
+print("Scissors..")
 
-import pandas as pd;
+player1 = input("Player 1, make your move: ")
+player2 = input("Player 2, make your move: ")
 
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.externals import joblib
-
-music_data = pd.read_csv('music.csv')
-X = music_data.drop(columns=['genre'])
-y = music_data['genre']
-
-model = DecisionTreeClassifier()
-model.fit(X, y)
-predictiions = model.predict([[21, 1], [22, 0]])
-print(predictiions)
