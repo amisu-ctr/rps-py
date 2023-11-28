ce67088834bf4bf3aa1d62964cff885b82121e92
amisu <hamisurahman@gmail.com>
2023-11-28 13:36:13 +0000

    Weeve selection wins

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import random
 
-# print("Rock..")
-# print("Paper...")
-# print("Scissors..")
+print("Rock..")
+print("Paper...")
+print("Scissors..")
 
-# player1 = input("Player 1, make your move: ")
+player = input("Player 1, make your move: ")
+
 rand_num = random.randint(0,2)
 if rand_num == 0:
     computer = 'rock'
@@ -15,25 +16,23 @@ else:
 print(computer)
 
 
-# player2 = input("Player 2, make your move: ")
-
-# if player1 == player2:
-# 	print("It's a tie!")
-# elif player1 == "rock":
-# 	if player2 == "scissors":
-# 		print("player1 wins!")
-# 	elif player2 == "paper":
-# 		print("player2 wins!")
-# elif player1 == "paper":
-# 	if player2 == "rock":
-# 		print("player1 wins!")
-# 	elif player2 == "scissors":
-# 		print("player2 wins!")
-# elif player1 == "scissors":
-# 	if player2 == "paper":
-# 		print("player1 wins!")
-# 	elif player2 == "rock":
-# 		print("player2 wins!")	
-# else:
-# 	print("something went wrong")
+if player == computer:
+	print("It's a tie!")
+elif player == "rock":
+	if computer == "scissors":
+		print("player wins!")
+	elif computer == "paper":
+		print("computer wins!")
+elif player == "paper":
+	if computer == "rock":
+		print("player wins!")
+	elif computer == "scissors":
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	elif computer == "rock":
+		print("computer wins!")	
+else:
+	print("something went wrong")
 
